b1553020f80d377a2540ee647dd53c55ff7175f6
Sfctech <40672985+sfctech@users.noreply.github.com>
2019-10-22 12:54:09 -0400

    Add files via upload
    Updated on 2019-10-22

--- app.js
+++ app.js
@@ -16,17 +16,22 @@ log4js.configure({
 });
 const logger = log4js.getLogger('AppConsole');
 
-const jsdom = require('jsdom');
-const { JSDOM } = jsdom;
-const { window } = new JSDOM();
-const { document } = (new JSDOM('')).window;
-global.document = document;
+const axios = require('axios');
 
-var $ = jQuery = require('jquery')(window);
 const express = require('express');
 const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 5000;
+
+// https://github.com/ericf/express-handlebars
+const exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey('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');
 
 app.use(express.static(path.join(__dirname)));  // point to root
 //app.use(express.static(path.join(__dirname + '/css')));
@@ -42,34 +47,57 @@ var cb1 = function (req, res, next) {
   next();
 }
 
-var cb2 = function (req, res) {
+var cb2 = function (req, res, next) {
   console.log('CB2');
-  res.send('Hello from C!');
+    //res.send('Hello from C!');
+    res.send(JSON.stringify({
+        message: "Hello from C!"
+    }));
+    next();
 }
 
-let getUrlIP = function () {
-    return new Promise(function (resolve, reject) {
-        $.getJSON('http://ip-api.com/json', function (data, textStatus, jqXHR) {
-            if (textStatus === "success") {
-                var ipInfo = "Access from [" + data.query + "] " + data.city + ", " + data.regionName + " " + data.country + ". ISP is " + data.isp + ", Location is " + data.lat + ", " + data.lon;
-                //console.log("Access from [%s] %s, %s %s. ISP is %s, location is %f, %f", data.query, data.city, data.regionName, data.country, data.isp, data.lat, data.lon);
-                //console.log(JSON.stringify(data, null, 2));
-                resolve(ipInfo);
-            } else {
-                reject("The errror status: " + textStatus);
-            }
-        });
+const sendMail = function (req, res) {
+    const msg = {
+        to: ['buccaneermontreal@gmail.com', 'sleunghome@gmail.com', 'luoxiaobin@gmail.com', 'kitchang1016@gmail.com', 'henrykwok212@gmail.com', 'ekwlau8@gmail.com', 'fktong2@gmail.com', 'dereksing16@outlook.com', 'lialbert298@gmail.com'],
+        from: 'sfctech2015@gmail.com',
+        subject: 'Noise from ' + req.query.name,
+        text: deHtmlEscape(decodeURIComponent(req.query.op)),
+        html: '<strong>' + deHtmlEscape(decodeURIComponent(req.query.op)) + '</strong>'
+    };
+    sgMail.send(msg);
+
+    //console.log('Send Mail via SendGrid');
+    //console.log('ACTUAL RESPONSE');
+    // ***this is a trip to prevent response twice via send json format***
+    //res.send(JSON.stringify({
+    //    message: "Your opinion is sent to SFC Admins successfully! Now you can close current Tab in your browser. (你可以关闭当前页面)"
+    //    //message: "Your opinion is sent to SFC Admins successfully! Now you can close current Tab in your browser."
+    //}));
+    //res.send('Sent an Email!');
+
+    res.render('message', {
+        message1: "Your opinion is sent to SFC Admins successfully!",
+        message2: "Now you can close current Tab in your browser. (" + deHtmlEscape(decodeURIComponent(req.query.hint)) + ")"
     });
 }
+
 
 let myLogger = function (req, res, next) {
-    getUrlIP().then((data, reject) => {
-        //let _msg = "[" + req.requestTime + "] " + data;
-        //successlog.info(_msg);
-        logger.info(data);
-        if (reject) logger.error(reject);
+    axios.get('http://ip-api.com/json')
+        .then(function (response) {
+            let data = response.data;
+            // handle success
+            let _msg = "Access from [" + data.query + "] " + data.city + ", " + data.regionName + " " + data.country + ". ISP is " + data.isp + ", Location is " + data.lat + ", " + data.lon;
+            //logger.info(_msg);
+        })
+        .catch(function (error) {
+            // handle error
+            logger.error(error);
+        })
+        .finally(function () {
+            // always executed
+        });
 
-    });
     next();
 }
 
@@ -81,6 +109,25 @@ let requestTime = function (req, res, next) {
                       replace(/\..+/, '');    // delete the dot and everything after
     next();
 }
+const deHtmlEscape = (str) => {
+    return String(str)
+        .replace(/&amp;/g, '&')
+        .replace(/&quot;/g, '"')
+        .replace(/&#39;/g, "'")
+        .replace(/&lt;/g, '<')
+        .replace(/&gt;/g, '>');
+}
+
+// function to prevent response twice
+app.use(function (req, res, next) {
+    //console.log("INTERCEPT-REQUEST");
+    const orig_send = res.send;
+    res.send = function (arg) {
+        //console.log("INTERCEPT-RESPONSE", JSON.stringify(arguments));
+        orig_send.call(res, arg);
+    };
+    next();
+});
 
 app.use(requestTime);
 app.use(myLogger);
@@ -110,6 +157,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'))
 })
     .get('/cb', [cb0, cb1, cb2])
+    .get('/sendmail', [sendMail])
     .get('/ec', (req, res) => res.sendFile(path.join(__dirname + '/emergency-contact.html')))
     .get('/2018fg', (req, res, next) => {
         res.sendFile(path.join(__dirname + '/2018FridayGroup.html'));
@@ -119,11 +167,19 @@ app.get('/', (req, res) => {
     .get('/assets', (req, res) => res.sendFile(path.join(__dirname + '/assets.html')))
     .get('/2018fr', (req, res) => res.sendFile(path.join(__dirname + '/2018FridayResult.html')))
     .get('/2018dome', (req, res) => res.sendFile(path.join(__dirname + '/2018DomeResult.html')))
+    .get('/2019Outdoor', (req, res) => res.sendFile(path.join(__dirname + '/2019OutdoorResult.html')))
+    .get('/2019dome', (req, res) => res.sendFile(path.join(__dirname + '/2019DomeResult.html')))
     .get('/jersey', (req, res) => res.sendFile(path.join(__dirname + '/jersey-size.html')))
     .get('/2018wcpool', (req, res) => res.sendFile(path.join(__dirname + '/wcpool2018.html')))
+    .get('/2019clpool', (req, res) => res.sendFile(path.join(__dirname + '/uefacl2019.html')))
+    .get('/2020clpool', (req, res) => res.sendFile(path.join(__dirname + '/uefacl2020.html')))
     .get('/stat2018fr', (req, res) => res.sendFile(path.join(__dirname + '/resultStatistic.html')))
     .get('/stat2018dome', (req, res) => res.sendFile(path.join(__dirname + '/2018DomeStatistic.html')))
+    .get('/stat2019Outdoor', (req, res) => res.sendFile(path.join(__dirname + '/2019OutdoorStatistic.html')))
+    .get('/stat2019dome', (req, res) => res.sendFile(path.join(__dirname + '/2019DomeStatistic.html')))
     .get('/face', (req, res) => res.sendFile(path.join(__dirname + '/facePlusPlus.html')))
+    .get('/avatar', (req, res) => res.sendFile(path.join(__dirname + '/avatar.html')))
+    .get('/opinion', (req, res) => res.sendFile(path.join(__dirname + '/opinion.html')))
     ;
    //.get('*', (req, res) => res.sendFile(path.join(__dirname + '/404.html')));
 
